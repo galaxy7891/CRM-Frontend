7eeab574791d08b09acdf2db653791b99b141d8c
Raafina <raafinuradzani16@gmail.com>
2024-10-24 11:28:53 +0700

    fix build

--- src/components/dark-mode.tsx
+++ src/components/dark-mode.tsx
@@ -1,27 +1,26 @@
-// hooks/useTheme.ts
-
-import { useEffect, useState } from "react";
+'use client';
+import { useEffect, useState } from 'react';
 
 const useTheme = () => {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    const isDark = savedTheme === "dark";
+    const savedTheme = localStorage.getItem('theme');
+    const isDark = savedTheme === 'dark';
     setIsDarkMode(isDark);
-    document.documentElement.classList.toggle("dark", isDark);
-    document.body.classList.toggle("dark", isDark); // Add this line
+    document.documentElement.classList.toggle('dark', isDark);
+    document.body.classList.toggle('dark', isDark); // Add this line
   }, []);
 
   const toggleTheme = () => {
     if (isDarkMode) {
-      document.documentElement.classList.remove("dark");
-      document.body.classList.remove("dark"); // Add this line
-      localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove('dark');
+      document.body.classList.remove('dark'); // Add this line
+      localStorage.setItem('theme', 'light');
     } else {
-      document.documentElement.classList.add("dark");
-      document.body.classList.add("dark"); // Add this line
-      localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add('dark');
+      document.body.classList.add('dark'); // Add this line
+      localStorage.setItem('theme', 'dark');
     }
     setIsDarkMode(!isDarkMode);
   };
