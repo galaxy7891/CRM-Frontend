349f6781746de35675647088feb4ef39e06e95c4
Farina Naswa <farinanaswa@gmail.com>
2024-12-07 09:28:51 +0700

    cms article

--- src/app/(cms)/(dashboard)/cms-article/new/page.tsx
+++ src/app/(cms)/(dashboard)/cms-article/new/page.tsx
@@ -1,23 +1,37 @@
+"use client";
+
 import DashboardCard from "@/components/layout/dashboard-card";
 import HeaderWithBackButton from "@/components/layout/header-with-back";
-import React from "react";
+import React, { useState } from "react";
 import ImageArticle from "./partials/image-article";
 import TitleArticle from "./partials/title-article";
 import SelectArticleStatus from "./partials/select-article-status";
+import Editor from "@/components/novel-rich-text/editor/editor";
+
+export const defaultValue = {
+  type: "doc",
+  content: [
+    {
+      type: "paragraph",
+      content: [],
+    },
+  ],
+};
 
 const NewArticle = () => {
+  const [content, setContent] = useState<string>("");
   return (
     <>
       <HeaderWithBackButton title="Tambah Artikel" />
       <DashboardCard>
         <div className="grid grid-cols-12 gap-6">
           {/* Bagian Upload Foto */}
-          <div className="col-span-12 lg:col-span-5 flex justify-center items-center">
+          <div className="col-span-12 lg:col-span-5">
             <ImageArticle />
           </div>
 
           {/* Bagian Input Judul Artikel */}
-          <div className="col-span-12 lg:col-span-7 flex flex-col justify-center">
+          <div className="col-span-12 lg:col-span-7 gap-4 flex flex-col justify-center">
             <TitleArticle
               label="Judul Artikel"
               required
@@ -34,6 +48,10 @@ const NewArticle = () => {
               // onChange={(e) => setLead({ ...lead, status: e.target.value })}
               required
             />
+            <p className="block text-xs md:text-base font-custom text-font-black dark:text-font-white">
+              Artikel
+            </p>
+            <Editor initialValue={defaultValue} onChange={setContent} />
           </div>
         </div>
       </DashboardCard>
