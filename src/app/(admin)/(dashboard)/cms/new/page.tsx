48574ea022820b4e76d7a1e7cf27bb7760614e32
Raafina <raafinuradzani16@gmail.com>
2025-01-19 11:10:14 +0700

    fix conflict

--- src/app/(admin)/(dashboard)/cms/new/page.tsx
+++ src/app/(admin)/(dashboard)/cms/new/page.tsx
@@ -1,25 +1,25 @@
-"use client";
+'use client';
 
-import { useDispatch } from "react-redux";
-import { addArticle } from "@/redux/actions/CMSActions";
-import { AppDispatch } from "@/redux/store";
-import DashboardCard from "@/components/layout/dashboard-card";
-import HeaderWithBackButton from "@/components/layout/header-with-back";
-import React, { useState, useRef, useEffect } from "react";
-import ImageArticle from "../partials/image-article";
-import TitleArticle from "../partials/title-article";
-import SelectArticleStatus from "../partials/select-article-status";
-import { articleTypes } from "@/types/CMSTypes";
-import SuccessModal from "@/components/status/success-modal";
-import FailText from "@/components/status/fail-text";
+import { useDispatch } from 'react-redux';
+import { addArticle } from '@/redux/actions/CMSActions';
+import { AppDispatch } from '@/redux/store';
+import DashboardCard from '@/components/layout/dashboard-card';
+import HeaderWithBackButton from '@/components/layout/header-with-back';
+import React, { useState, useRef, useEffect } from 'react';
+import ImageArticle from '../partials/image-article';
+import TitleArticle from '../partials/title-article';
+import SelectArticleStatus from '../partials/select-article-status';
+import { articleTypes } from '@/types/CMSTypes';
+import SuccessModal from '@/components/status/success-modal';
+import FailText from '@/components/status/fail-text';
 
 const NewArticle = () => {
   const [photo, setPhoto] = useState<File | null>(null);
   const [preview, setPreview] = useState<string | null>(null);
-  const [content, setContent] = useState<string>("");
+  const [content, setContent] = useState<string>('');
   const [article, setArticle] = useState<articleTypes>({
-    title: "",
-    status: "",
+    title: '',
+    status: '',
   });
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isSuccess, setIsSuccess] = useState<boolean>(false);
@@ -59,7 +59,7 @@ const NewArticle = () => {
 
     if (trixElement) {
       trixElement.addEventListener(
-        "trix-change",
+        'trix-change',
         handleTrixChange as EventListener
       );
     }
@@ -67,7 +67,7 @@ const NewArticle = () => {
     return () => {
       if (trixElement) {
         trixElement.removeEventListener(
-          "trix-change",
+          'trix-change',
           handleTrixChange as EventListener
         );
       }
@@ -99,9 +99,9 @@ const NewArticle = () => {
               label="Status Artikel"
               value={article.status}
               options={[
-                { label: "Pilih Status Artikel", value: "", hidden: true },
-                { label: "Terbit", value: "Terbit" },
-                { label: "Draf", value: "Draf" },
+                { label: 'Pilih Status Artikel', value: '', hidden: true },
+                { label: 'Terbit', value: 'Terbit' },
+                { label: 'Draf', value: 'Draf' },
               ]}
               onChange={(e) =>
                 setArticle({ ...article, status: e.target.value })
@@ -119,7 +119,6 @@ const NewArticle = () => {
             {errorMessage && <FailText>{errorMessage.description}</FailText>}
             <div className="flex justify-end mt-4">
               <button
-                disabled={isLoading}
                 type="submit"
                 onClick={handleAddArticle}
                 disabled={isLoading}
